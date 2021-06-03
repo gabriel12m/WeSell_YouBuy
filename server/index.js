81b886b10048437edbaa3083115eb68ae1d14bb6
gabriel12m <2000marcal@gmail.com>
2021-06-03 04:09:20 +0100

    Adição da Tabela "Utilizadores" à Base de Dados

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const db = require("./models");
+
+//Routers
+const utilizadoresRouter = require("./routes/Utilizadores");
+app.use("/utilizadores", utilizadoresRouter);
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
+});
---
+++ server/models/Utilizadores.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const Utilizadores = sequelize.define("Utilizadores", {
+    idUtilizador: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    datanascimento: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    sexo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contacto: {
+      type: DataTypes.STRING(13),
+      allowNull: false,
+    },
+    morada: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    cod_postal: {
+      type: DataTypes.STRING(8),
+      allowNull: false,
+    },
+    NIF: {
+      type: DataTypes.STRING(9),
+      allowNull: false,
+    },
+  });
+
+  return Utilizadores;
+};
+
+// TODO : Adicionar password e roles
